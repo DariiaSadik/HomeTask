908314c79dbc6c0333ac50a2f85e597a3c6a4dd3
DariiaSadik <dasha.sadik@gmail.com>
2014-10-30 20:57:00 +0200

    Task01 -> test11

--- src/com/homeTask/task02/Test11.java
+++ src/com/homeTask/task02/Test11.java
@@ -2,5 +2,11 @@ package com.homeTask.task02;
 
 public class Test11 {
     public static void main (String[] args){
+        byte age = 21;    //00010101
+        int height = 165;//10100101
+        byte weight = 2;  //00000010                         // :)))
+
+        int conteiner = (age) | (height << 8) | (weight << 16);
+        System.out.println(conteiner);
     }
 }
