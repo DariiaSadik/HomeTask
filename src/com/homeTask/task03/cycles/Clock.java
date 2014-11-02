0caadf6996fefe477e97e139fc765a9a1d6b618a
DariiaSadik <dasha.sadik@gmail.com>
2014-11-03 00:23:15 +0200

    New Class Clock -> Test04

--- src/com/homeTask/task03/cycles/Clock.java
+++ src/com/homeTask/task03/cycles/Clock.java
@@ -1,83 +1,67 @@
 package com.homeTask.task03.cycles;
-
+// isMidnight
+// isMirrored -> str.charAt(i);
 public class Clock {
-    String hours;
-    String minute;
+    String minute = "00";
+    String  hours = "00";
+    int count = 0; // количество isMirrored()
 
-    void tick (){
-        if(Integer.parseInt(minute) >= 60){
+    // минуты ++
+    void tick() {
+        if (Integer.parseInt(minute) < 10) {
+            minute = (Integer.parseInt(minute) + 1) + "";
+            minute = Half(minute);
+        } else {
+            minute = (Integer.parseInt(minute) + 1) + "";
+        }
+        if (Integer.parseInt(minute) == 60){
+            minute = "00";
+            hours();
+        }
+        printAll();
+    }
+    // часы ++
+    void hours(){
+        if (Integer.parseInt(hours) < 10){
             hours = (Integer.parseInt(hours) + 1) + "";
+            hours = Half(hours);
+        }
+        else {
+            hours = (Integer.parseInt(hours) + 1) + "";
+            isMidnight();
+        }
+    }
+    // сутки
+    void isMidnight(){
+        if (Integer.parseInt(hours) == 24){
+            hours = "00";
             minute = "00";
         }
     }
-    boolean isMirrored () {
-        return ((Integer.parseInt(hours) / 10 == Integer.parseInt(minute) % 10) && (Integer.parseInt(hours) % 10 == Integer.parseInt(minute) / 10));
-//        if ((Integer.parseInt(hours) / 10 == Integer.parseInt(minute) % 10) && (Integer.parseInt(hours) % 10 == Integer.parseInt(minute) / 10))
-//            return true;
-//            else return false;
+    // минуты или часы
+    String Half (String str){
+        String firstHalf = "";
+        int index = 2 - (str.length());
+        for (int i = index; i > 0; i--){
+            firstHalf += "0";
+        }
+        return firstHalf + str;
     }
-    void count () {
-        String h;
-        String m;
-        int count = 0;
-        for(int i = 0; i < 24; i++){
-            h = "0";
-            h += i;
-            hours = h;
-            //h += ":";
-            for (int j = 0; j <= 60; j++){
-                m = "0";
-                m += j;
-                minute = m;
-                // hm = h + m;
-                //System.out.println(hm);
-                tick();
-                isMirrored();
-                if (isMirrored()) {
-                    count++;
-                    String s = (hours.substring(1) + " " + minute.replaceFirst("0", "")).replace(" ", ":");
-                    System.out.println(s + " " + count);
-                }
-            }
+    boolean isMirrored(){
+        return ((Integer.parseInt(hours) / 10 == Integer.parseInt(minute) % 10) &&
+                (Integer.parseInt(minute) / 10 == Integer.parseInt(hours) % 10));
+    }
+    void printAll(){
+        if (isMirrored()) {
+            count++;
+            System.out.println(hours + ":" + minute + "  -  " + count);
         }
     }
     public static void main(String[] args){
+        System.out.println(" Time " + "  index");
         Clock clock = new Clock();
-        clock.count();
+        for (int i = 0; i < 2000; i++) {
+            clock.tick();
+        }
     }
 }
-
-//clock.hours = "05";
-//clock.minute = "50";
-//        String h = "0";
-//        String m = "0";
-//        int count = 0;
-//        for(int i = 0; i < 24; i++){
-//            h = "0";
-//            h += i;
-//            clock.hours = h;
-//            //h += ":";
-//            for (int j = 0; j <= 60; j++){
-//                m = "0";
-//                m += j;
-//                clock.minute = m;
-//               // hm = h + m;
-//                //System.out.println(hm);
-//                clock.tick();
-//                clock.isMirrored();
-//               // int count = 0;
-//                if (clock.isMirrored() == true) {
-//                    count++;
-//                    System.out.println(clock.hours + " " + clock.minute + " " + count);
-//                }
-//            }
-//        }
-//        clock.tick();
-//        clock.isMirrored();
-//        int count = 0;
-//        if (clock.isMirrored() == true){
-//            count++;
-//            System.out.println(clock.hours + " " + clock.minute + " " + count);
-//        } else {
-//            System.out.println("0");
-//        }
