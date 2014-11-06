aed003b00150932b8adc956d966c589d9628c239
DariiaSadik <dasha.sadik@gmail.com>
2014-11-06 20:05:40 +0200

    recursion

--- src/com/homeTask/task03/recursion/Test02.java
+++ src/com/homeTask/task03/recursion/Test02.java
@@ -1,6 +1,7 @@
 package com.homeTask.task03.recursion;
 
 public class Test02 {
+    static int c;
     static int fibonacci(int n){
         if (n == 0)
             return 1;
@@ -8,14 +9,12 @@ public class Test02 {
             if (n == 1)
                 return 1;
             else
+            if (n == 4){
+                c++;
+            }
                 return fibonacci(n - 1) + fibonacci(n - 2);
     }
-    static int four(int n){
-        if (n > 4) return fibonacci(n)/fibonacci(4);
-        else return 0;
-    }
     public static void main(String[] args){
-        System.out.println(fibonacci(15) + " is number of Fibonacci");
-        System.out.println(four(15) + " raz");
+        System.out.println(fibonacci(15) + " is number of Fibonacci " + Test02.c);
     }
 }
