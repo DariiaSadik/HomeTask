b40a9a0a4febdeaf7f3aa24f7976dd22fa1c96ca
DariiaSadik <dasha.sadik@gmail.com>
2014-12-07 16:12:01 +0200

    Map/Set

--- src/com/sourceit/hometask/collections/MultiValueMapImpl.java
+++ src/com/sourceit/hometask/collections/MultiValueMapImpl.java
@@ -2,61 +2,91 @@ package com.sourceit.hometask.collections;
 
 import java.util.*;
 
-public class MultiValueMapImpl<K, V> implements MultiValueMap{
-    Map<K, V> mapArr = new HashMap<>();
+public class MultiValueMapImpl<K, V> implements MultiValueMap<K, V>{
+    private Map<K, List<V>> mapArr = new HashMap<>();
 
     @Override
-    public int getCountValues(Object key) {
-        return mapArr.size();
+    public int getCountValues(K k) {
+        int count = 0;
+        if(mapArr.containsValue(k)){
+            count  = mapArr.get(k).size();
+        }
+        return count;
     }
 
     @Override
     public int size() {
-        if (mapArr == null) return 0;
-        else return mapArr.size();
+        return mapArr.size();
     }
 
     @Override
     public boolean isEmpty() {
-        if(mapArr.isEmpty()) return true;
-        else return false;
+        return mapArr.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return mapArr.containsKey(key);
+        if (mapArr.containsKey(key)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return mapArr.containsValue(value);
+        for (K key : mapArr.keySet()) {
+            List<V> values = mapArr.get(key);
+            if (values.contains(value)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
-    public Object get(Object key) {
-        return mapArr.get(key);
+    public V get(Object o) {
+        V var = null;
+        if(mapArr.containsValue(o)){
+            List<V> value = mapArr.get(o);
+            var = value.get(value.size() - 1);
+        }
+        return var;
     }
 
     @Override
-    public Object put(Object key, Object value) {
-        if((key == null) || (value == null)) throw new IllegalArgumentException();
-        return mapArr.put((K) key, (V) value);
+    public V put(K key, V value) {
+        if(mapArr.containsKey(key)){
+            List<V> values = mapArr.get(key);
+            values.add(value);
+        } else {
+            List<V> val = new ArrayList<>();
+            val.add(value);
+            mapArr.put(key, val);
+        }
+        return value;
     }
 
     @Override
-    public Iterator getIterator(Object key) {
+    public Iterator<V> getIterator(K k) {
+        List<V> value = mapArr.get(k);
+        if(value != null) {
+            return value.iterator();
+        }
         return null;
-    } // create Iterator and all method and return Iterator
-
-    @Override
-    public Object remove(Object key) {
-        if(!containsKey(key)) return "";
-        else return mapArr.remove(key);
     }
 
     @Override
-    public void putAll(Map m) {
-        if (!m.isEmpty()) mapArr.putAll(m);
+    public V remove(Object o) {
+        V value = get(o);
+        mapArr.remove(o);
+        return value;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        for(K key : m.keySet()){
+            put(key, m.get(key));
+        }
     }
 
     @Override
@@ -65,19 +95,44 @@ public class MultiValueMapImpl<K, V> implements MultiValueMap{
     }
 
     @Override
-    public Set keySet() {
+    public Set<K> keySet() {
         return mapArr.keySet();
     }
 
     @Override
-    public Collection values() {
-        List<V> valueList = new ArrayList<>();
-        valueList.addAll(mapArr.values());
-        return valueList;
+    public Collection<V> values() {
+        Collection<V> values = new ArrayList<>();
+        for (K key : mapArr.keySet()) {
+            values.addAll(mapArr.get(key));
+        }
+        return values;
     }
 
     @Override
-    public Set<Entry> entrySet() {
-        return null;
-    } // input key and value
+    public Set<Entry<K, V>> entrySet() {
+        Set<Entry<K, V>> entries = new LinkedHashSet<>();
+//        for(K key : mapArr.entrySet()){
+//            List<V> value = mapArr.get(key);
+//
+//            entries.add(key, value.get(value.size() - 1));
+//        }
+         // V -> значение последнее элемента в List
+        return entries;
+    }
+
+    @Override
+    public String toString() {
+        return "MultiValueMapImpl{" +
+                "mapArr=" + mapArr +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        MultiValueMapImpl<Integer, String> valueMap = new MultiValueMapImpl<>();
+        valueMap.put(1, "one");
+        valueMap.put(2, "two");
+        valueMap.put(3, "tree");
+        valueMap.put(4, "four");
+        System.out.println(valueMap);
+    }
 }
--- src/com/sourceit/hometask/collections/SetUtilsImpl.java
+++ src/com/sourceit/hometask/collections/SetUtilsImpl.java
@@ -7,24 +7,20 @@ public class SetUtilsImpl implements SetUtils {
     @Override
     public SortedSet<String> orderedSet(Collection<Integer> integers, Set<String> strings) throws NullPointerException {
         if(integers == null || strings == null) throw new NullPointerException();
-        Integer[] intArray = integers.toArray(new Integer[integers.size()]);
+
         String[] strArray = strings.toArray(new String[strings.size()]);
-        SortedSet<String> sortedSet = new TreeSet();
-        for (int i = intArray.length - 1; i >= 0; i--){
-            for (int j = 0; j < i; j++){
-                if(intArray[j] > intArray[j + 1]){
-                    Integer tempInt = intArray[i];
-                    String tempStr = strArray[i];
-                    intArray[i] = intArray[j];
-                    strArray[i] = strArray[j];
-                    intArray[j] = tempInt;
-                    strArray[j] = tempStr;
-                }
+        Integer[] intArray = integers.toArray(new Integer[integers.size()]);
+        final Map<String, Integer> map = new HashMap<>();
+        for (int i = 0 ; i < integers.size(); i++){
+            map.put(strArray[i], intArray[i]);
+        }
+        SortedSet<String> sortedSet = new TreeSet(new Comparator<String>() {
+            @Override
+            public int compare(String str, String str2) {
+                return map.get(str).compareTo(map.get(str2));
             }
-        }
-        for (int i = 0; i < strArray.length; i++){
-            sortedSet.add(strArray[i]);
-        }
+        });
+        sortedSet.addAll(strings);
         return sortedSet;
     }
 
@@ -36,5 +32,26 @@ public class SetUtilsImpl implements SetUtils {
             customOrderSet.add(integers[i]);
         }
         return customOrderSet;
+    }
+
+    public static void main(String[] args) {
+        SetUtilsImpl order = new SetUtilsImpl();
+
+        Collection<Integer> value = new ArrayList<>();
+        value.add(1);
+        value.add(3);
+        value.add(5);
+        value.add(7);
+        value.add(11);
+        value.add(9);
+
+        Set<String> key = new LinkedHashSet<>();
+        key.add("a");
+        key.add("y");
+        key.add("u");
+        key.add("q");
+        key.add("i");
+        key.add("p");
+        System.out.println(order.orderedSet(value, key).toString());
     }
 }
